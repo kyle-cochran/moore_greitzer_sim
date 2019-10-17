aa17a35c999c5164d58c3146547ffefe830a0805
Kyle C <afiftyfootjello@gmail.com>
2019-10-17 14:16:57 -0500

    started data manipulation tasks

---
+++ mathematica/plotdata.py
@@ -0,0 +1,44 @@
+#!/bin/python
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+gData = list()
+
+numthetas = 7
+numts = 21
+
+with open("gData.csv", 'r') as gf:
+    for num in gf:
+        gData.append(float(num.strip()))
+
+gData = np.asarray(gData)
+gData = np.reshape(gData,(numthetas,numts))
+
+thetas = np.arange(numthetas)
+ts = np.arange(numts)
+
+thetaMat = list()
+tMat = list()
+for theta in thetas:
+    thetaMat.append(theta * np.ones(numts))
+    
+    tMat.append(ts)
+
+#thetaMat = np.transpose(thetaMat)
+print(tMat)
+print(thetaMat)
+print(gData)
+    
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+print(gData.shape)
+print(ts.shape)
+print(thetas.shape)
+ax.plot_surface(tMat, thetaMat, gData)
+
+plt.show()
